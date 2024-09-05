faf9fa817991e1721152b1dda54c7fd8e477d233
@thespeedofYES <113219452+Jas-zh@users.noreply.github.com>
2024-09-05 10:31:38 +0800

    3d art work code

---
+++ 3dart.py
@@ -0,0 +1,28 @@
+import turtle
+
+t = turtle.Turtle()
+sc = turtle.Screen
+i = 0
+
+def tright():
+    t.forward(100)
+    t.right(135)
+
+t.left(90)
+tright()
+
+t.forward(100)
+t.right(45)
+tright()
+ 
+i = 0
+while i < 2:
+ t.forward(100)
+ t.right(45)
+ i+=1
+
+t.right(45)
+t.forward(150)
+t.right(135)
+
+t.forward(100)
