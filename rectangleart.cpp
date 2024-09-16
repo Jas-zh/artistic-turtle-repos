0980a0683ed5f04b6e5af2ab93d0fc5346c91f01
@thespeedofYES <113219452+Jas-zh@users.noreply.github.com>
2024-09-16 09:40:52 +0800

    .....

---
+++ rectangleart.cpp
@@ -0,0 +1,25 @@
+#include <CTurtle.hpp>
+
+namespace ct = cturtle ; 
+
+int main(){
+  ct::TurtleScreen scr;
+  ct::Turtle t(scr);
+  t.speed(ct::TS_SLOWEST);
+  t.fillcolour({"blue"});
+  for(int e = 0, e < 2, e++){
+  t.pen_down();
+  t.begin_fill();
+  for(int i = 0, i < 4, i++){
+    t.forward(100);
+    t.right(90);
+  }
+  t.end_fill();
+  t.pen_up();
+  t.forward(100);
+  }
+  t.right(90);
+
+  scr.bye();
+  return 0;
+}
