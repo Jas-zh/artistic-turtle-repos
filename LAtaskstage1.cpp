b9f4442c4e883dd723b1eac2b61bbd88224f70f0
@thespeedofYES <113219452+Jas-zh@users.noreply.github.com>
2024-09-16 09:47:18 +0800

    functionified code

--- LAtaskstage1.cpp
+++ LAtaskstage1.cpp
@@ -2,20 +2,34 @@
 
 namespace ct = cturtle ; 
 
+void square(){
+  ct::TurtleScreen scr;
+  ct::Turtle t(scr);
+  t.speed(ct::TS_SLOWEST);
+ for(int sq = 0, sq < 4, sq++){
+  t.forward(100)
+  t.right(90);
+ }
+}
+ 
+void penup(){
+  t.pen_down();
+  t.begin_fill();
+}
+
+void pendown(){
+  t.end_fill();
+  t.pen_up();
+}
 int main(){
   ct::TurtleScreen scr;
   ct::Turtle t(scr);
   t.speed(ct::TS_SLOWEST);
   t.fillcolour({"blue"});
   for(int e = 0, e < 2, e++){
-  t.pen_down();
-  t.begin_fill();
-  for(int i = 0, i < 4, i++){
-    t.forward(100);
-    t.right(90);
-  }
-  t.end_fill();
-  t.pen_up();
+  penup();
+  square();
+  pendown();
   }
   scr.bye();
   return 0;
