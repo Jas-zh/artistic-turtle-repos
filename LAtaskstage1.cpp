74dd88788e885f149f59b438a3fcd0dfa74fd2e1
@thespeedofYES <113219452+Jas-zh@users.noreply.github.com>
2024-09-16 09:52:05 +0800

    made functions more efficient

--- LAtaskstage1.cpp
+++ LAtaskstage1.cpp
@@ -6,30 +6,24 @@ void square(){
   ct::TurtleScreen scr;
   ct::Turtle t(scr);
   t.speed(ct::TS_SLOWEST);
+  t.pen_down();
+  t.begin_fill();
  for(int sq = 0, sq < 4, sq++){
   t.forward(100)
   t.right(90);
  }
-}
- 
-void penup(){
-  t.pen_down();
-  t.begin_fill();
+ t.end_fill();
+  t.pen_up();
+
 }
 
-void pendown(){
-  t.end_fill();
-  t.pen_up();
-}
 int main(){
   ct::TurtleScreen scr;
   ct::Turtle t(scr);
   t.speed(ct::TS_SLOWEST);
   t.fillcolour({"blue"});
   for(int e = 0, e < 2, e++){
-  penup();
   square();
-  pendown();
   }
   scr.bye();
   return 0;
