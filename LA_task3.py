c9d79dc6b042b03051a28342538ae26fa555aad0
@thespeedofYES <113219452+Jas-zh@users.noreply.github.com>
2024-09-25 10:11:46 +0800

    updated code ton fit requirements

--- LA_task3.py
+++ LA_task3.py
@@ -5,6 +5,7 @@ t = turtle.Turtle()
 sc = turtle.Screen()
 sc.bgcolor('RoyalBlue3')
 sc.setup(600,600)
+sc.tracer(20)
 
 t.pensize(10)
 t.fillcolor("silver")
@@ -40,8 +41,8 @@ def spiral():
         dist += 10
         width = random.randint(0,5)
 def anotherspiral():
-    dist2 = 10
-    width2 = 1
+    dist2 = 0.01
+    width2 = 0.1
     colorpicker = random.randint(0,2)
     if colorpicker == 0:
         colour = "blue4"
@@ -50,7 +51,7 @@ def anotherspiral():
     else:
         colour = "black"
      
-    for i in range (50):
+    for i in range (1000):
         if colorpicker == 0:
             colour = "blue4"
         elif colorpicker == 1:
@@ -60,18 +61,38 @@ def anotherspiral():
         t.forward(dist2)
         t.pensize(width2)
         t.pencolor(colour)
-        t.right(45)
-        dist2 += 10
-        width2 = random.randint(0,10)
+        t.right(1)
+        dist2 += 0.01
+        width2 += 0.1
 def snowflakes():
-    t.fillcolor(white)
-    t.begin_fill()
-    for i in range (10):
-        t.circle(5)
+    t.home()
+    t.pencolor("white")
+    t.pensize(19)
+    for i in range (15):
+        t.pendown()
+        t.circle(10)
+        t.penup()
         x = random.randint(-300,300)
         y = random.randint(-300,300)
         t.goto(x,y)
+
+    
+def starwithpent():
+    t.pensize(10)
+    t.fillcolor("yellow")
+    t.pencolor("grey")
+    t.home()
+    t.begin_fill()
+    for i in range (5):
+        t.forward(100)
+        t.left(72)
     t.end_fill()
+    
+def penupdown():
+    t.penup()
+    t.home()
+    t.pendown()
+    
 
 square()
 t.forward(150)
@@ -92,9 +113,13 @@ antisquare()
 
 spiral()
 
-t.penup()
-t.home()
-t.pendown()
+penupdown()
 
 anotherspiral()
+penupdown()
+snowflakes()
+penupdown()
+starwithpent()
+
+
 
