0cdf6e860fa38ce446a8862130954791cb8c9703
@thespeedofYES <113219452+Jas-zh@users.noreply.github.com>
2024-10-28 10:41:42 +0800

    Updated Technical skill submission
    This will be a showcase of my technical knowledge of turtle, and will be submitted along with project.py

--- archlinux.py
+++ archlinux.py
@@ -1,3 +1,4 @@
+# Set up
 import turtle
 import random
 import time
@@ -10,16 +11,31 @@ sc.tracer(20)
 
 t.left(180)
 
+#Setup
 t.penup()
 t.forward(150)
 t.right(110)
 
+#Frame code i stole
+def frame():
+    t.pencolor("blue")
+    t.up()
+    t.goto(350, -350)   
+    t.pensize(10)
+    t.down()
+    t.goto(350, 350)    
+    t.goto(-350, 350)   
+    t.goto(-350, -350)  
+    t.goto(350, -350)
+
+# Cool Sqaure spiral
 def squaredows():
    t.pendown()
    t.fillcolor(random.randint(0,1),random.randint(0,1),random.randint(0,1))
    t.pencolor(random.randint(0,1),random.randint(0,1),random.randint(0,1))
    t.circle(55, steps=4)
 
+#Cool square spiral cont.
 def squarespiral():
     t.penup()
     t.goto(random.randint(-300,300),random.randint(-300,300))
@@ -28,7 +44,7 @@ def squarespiral():
         squaredows()
         t.forward(25)
     
-
+#Geometric Logo of Arch Linux
 def archlinux():
     t.pendown()
     t.fillcolor("#4592cc")
@@ -48,11 +64,26 @@ def archlinux():
     t.forward(105)
     t.penup()
 
+# Cool shapes
+def randeverything():
+    for i in range(1):
+        t.begin_fill()
+        t.penup()
+        t.pencolor(random.random(), random.random(), random.random())
+        t.fillcolor(random.random(), random.random(), random.random())
+        t.goto(random.randint(-300,300), random.randint(-300,300))
+        t.pendown()
+        t.circle(radius= 80, steps= random.randint(3,10))
+        t.end_fill()
+        
+
+#Animation code
 a=0
 while a < 1:
     a = 0
     sc.tracer(20)
     sc.bgcolor("black")
+    frame()
     for i in range(6):
         t.pendown()
         archlinux()
@@ -64,8 +95,10 @@ while a < 1:
         t.pencolor(random.randint(0,1),random.randint(0,1),random.randint(0,1))
         t.pendown()
         t.write("Jason", font=("Trebuchet MS bold", 50))
+        randeverything()
         t.penup()
         t.goto(random.randint(-300,300),random.randint(-300,300))
+    frame()
     time.sleep(1)
     sc.update()
     sc.clear()
